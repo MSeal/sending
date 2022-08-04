2cd5b0e96161d54b1d4eaf8587251a3b12c4a34b
Matt Kafonek <matt.kafonek@noteable.io>
2022-08-04 05:56:23 -0400

    OSSify Sending (#20)
    * OSSify Sending
    
    * changelog
    
    * absolutely mangled poetry lock rebase, so deleted and recreated with poetry install

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,1 @@
-import pytest
-
 pytest_plugins = "managed_service_fixtures"
-
-
-def pytest_collection_modifyitems(config, items):
-    keywordexpr = config.option.keyword
-    markexpr = config.option.markexpr
-    if keywordexpr or markexpr:
-        return  # let pytest handle this
-
-    for item in items:
-        if "redis" in item.keywords:
-            item.add_marker(pytest.mark.skip(reason="redis tests not enabled"))
-        if "jupyter" in item.keywords:
-            item.add_marker(pytest.mark.skip(reason="jupyter tests not enabled"))
