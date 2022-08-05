75937dcfac294392f2243ddf422fff160625d7fa
Kafonek <matt.kafonek@noteable.io>
2022-08-05 09:35:19 -0400

    nox use -E all

--- noxfile.py
+++ noxfile.py
@@ -9,9 +9,7 @@ nox.options.sessions = ["lint", "test"]
 
 @nox_poetry.session(python=["3.8", "3.9", "3.10"])
 def test(session: nox_poetry.Session):
-    session.run_always(
-        "poetry", "install", "-E", "redis", "-E", "jupyter", "-E", "websockets", external=True
-    )
+    session.run_always("poetry", "install", "-E", "all", external=True)
     session.run(
         "pytest",
         "-v",
